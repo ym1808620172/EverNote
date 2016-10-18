e3955006a4846b5bc71b484e9417d8ab1a3e523c
huxinghuas <dllo@huxinghua.local>
2016-10-18 17:46:34 +0800

    no message

--- EverNote/app/src/main/java/come/evernote/evernote/controler/activity/MainActivity.java
+++ EverNote/app/src/main/java/come/evernote/evernote/controler/activity/MainActivity.java
@@ -1,11 +1,15 @@
 package come.evernote.evernote.controler.activity;
 
 
+import android.os.Build;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import come.evernote.evernote.R;
 import come.evernote.evernote.controler.fragment.TextNotesFragment;
@@ -15,6 +19,8 @@ public class MainActivity extends AbsBaseActivity implements View.OnClickListene
     private FragmentManager manager;//管理者
     private FragmentTransaction transaction;
 
+    private DrawerLayout rootView;// 整个页面的布局对象
+    private LinearLayout layout;// 抽屉布局对象
     @Override
     protected int setLayout() {
         return R.layout.activity_main;
@@ -24,10 +30,22 @@ public class MainActivity extends AbsBaseActivity implements View.OnClickListene
     protected void initView() {
         imageView = byView(R.id.main_img);
         imageView.setOnClickListener(this);
+        rootView = byView(R.id.root_view);
+        layout  = byView(R.id.drawer_view);
     }
 
     @Override
     protected void initDatas() {
+
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            //透明状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //透明导航栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+
+
 
     }
 
@@ -43,6 +61,7 @@ public class MainActivity extends AbsBaseActivity implements View.OnClickListene
     }
     protected void onClickDrawer() {
 
+        rootView.openDrawer(layout);
     }
 
     @Override
