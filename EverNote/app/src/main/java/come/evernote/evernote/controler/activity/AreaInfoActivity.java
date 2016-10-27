4087068a3065b94269e5d543c080abb509588a30
huxinghuas <dllo@huxinghua.local>
2016-10-27 19:15:22 +0800

    no message

---
+++ EverNote/app/src/main/java/come/evernote/evernote/controler/activity/AreaInfoActivity.java
@@ -0,0 +1,17 @@
+package come.evernote.evernote.controler.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import come.evernote.evernote.R;
+
+/**
+ * Created by dllo on 16/10/27.
+ */
+public class AreaInfoActivity  extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_areainfo);
+    }
+}
