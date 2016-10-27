c3d2f54778e4bde8189637a23e4bc7b00b735938
huxinghuas <dllo@huxinghua.local>
2016-10-27 20:59:33 +0800

    二级列表

---
+++ EverNote/app/src/main/java/come/evernote/evernote/controler/activity/AreInfoActivity.java
@@ -0,0 +1,16 @@
+package come.evernote.evernote.controler.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dllo on 16/10/27.
+ */
+public class AreInfoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
